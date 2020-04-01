2e8a2322994148f273a634ba048eb4f537392266
Sergey Darvay <sdarvay@ld.yandex.ru>
2020-04-01 18:01:15 +0300

    '2nd_task'

---
+++ document-structure/todo/task.js
@@ -0,0 +1,24 @@
+const tasks__list = document.querySelector('div.tasks__list');
+let tasks = document.querySelectorAll('a.task__remove');
+
+document.querySelector('#tasks__add').addEventListener('click', (event) => {
+  let currValue = document.querySelector('#task__input');
+  let newTask = document.createElement('div');
+  newTask.classList.add('task_container');
+  newTask.innerHTML = ('<div class="task"><div class="task__title">' + currValue.value + '</div><a href="#" class="task__remove">&times;</a></div>');
+  tasks__list.appendChild(newTask);
+  currValue.value = '';
+  tasks = document.querySelectorAll('a.task__remove');
+  tasks.forEach(function(task) {
+    task.addEventListener('click', taskDeleteHandler);
+  });
+});
+
+const taskDeleteHandler = function(event) {
+  let task = event.target;
+  task.parentElement.parentElement.remove(); 
+};
+
+tasks.forEach(function(task) {
+  task.addEventListener('click', taskDeleteHandler);
+});
