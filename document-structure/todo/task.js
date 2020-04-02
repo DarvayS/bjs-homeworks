3fa952477cf2031b6c5d67505b3cd6fde8d9c582
Sergey Darvay <sdarvay@ld.yandex.ru>
2020-04-02 15:12:18 +0300

    'task.js_edited'

--- document-structure/todo/task.js
+++ document-structure/todo/task.js
@@ -3,16 +3,18 @@ let tasks = document.querySelectorAll('a.task__remove');
 
 document.querySelector('#tasks__add').addEventListener('click', (event) => {
   let currValue = document.querySelector('#task__input');
-  let newTask = document.createElement('div');
-  newTask.classList.add('task_container');
-  newTask.innerHTML = ('<div class="task"><div class="task__title">' + currValue.value + '</div><a href="#" class="task__remove">&times;</a></div>');
-  tasks__list.appendChild(newTask);
-  currValue.value = '';
-  event.preventDefault();  
-  tasks = document.querySelectorAll('a.task__remove'); 
-  tasks.forEach(function(task) {
-    task.addEventListener('click', taskDeleteHandler);
-});
+  if (currValue.value != '') {
+    let newTask = document.createElement('div');
+    newTask.classList.add('task_container');
+    newTask.innerHTML = ('<div class="task"><div class="task__title">' + currValue.value + '</div><a href="#" class="task__remove">&times;</a></div>');
+    tasks__list.appendChild(newTask);
+    currValue.value = '';
+    event.preventDefault();  
+    tasks = document.querySelectorAll('a.task__remove'); 
+    tasks.forEach(function(task) {
+      task.addEventListener('click', taskDeleteHandler);
+    });
+  };
 });
 
 const taskDeleteHandler = function(event) {
