5d382eda547b879c7e8ea8110d9d7aefc8b8ad19
Sergey Darvay <sdarvay@ld.yandex.ru>
2020-04-25 15:53:55 +0300

    '3d_task'

---
+++ document-structure/cart/task.js
@@ -0,0 +1,53 @@
+const inc = document.querySelectorAll('div.product__quantity-control.product__quantity-control_inc');
+const dec = document.querySelectorAll('div.product__quantity-control.product__quantity-control_dec');
+const addToCarts = document.querySelectorAll('div.product__add');
+const cart = document.querySelector('div.cart__products');
+
+
+const incHandler = function(event) {
+  let val = event.target;
+  val.previousElementSibling.textContent = +(val.previousElementSibling.textContent) + 1;
+}
+
+const decHandler = function(event) {
+  let val = event.target;
+  if (val.nextElementSibling.textContent > 0)
+    val.nextElementSibling.textContent = +(val.nextElementSibling.textContent) - 1; 
+}
+
+const addProduct = function(event) {
+  let newCart = event.target;
+  let curProductId = newCart.closest('div.product').dataset.id;
+  let curProduct = newCart.closest('div.product');
+  let newProd = document.createElement('div');
+  newProd.className = "cart__product";
+  newProd.dataset.id = curProductId;
+  newProd.innerHTML = '<img class="cart__product-image" src=' + curProduct.querySelector('.product__image').src + '> <div class="cart__product-count">' + +(curProduct.querySelector('.product__quantity-value').textContent) +'</div>';
+  let alreadyExist = 0;
+  let elemId;
+  for (i = 0; i < cart.children.length; i++) {
+    if (cart.children[i].dataset.id === curProductId) {
+      alreadyExist = 1;
+      elemId = i;
+      break;
+    }
+  }
+  if (alreadyExist === 0) {
+    cart.appendChild(newProd);
+  }
+  else {
+    cart.children[i].querySelector('.cart__product-count').textContent = +cart.children[i].querySelector('.cart__product-count').textContent + +(curProduct.querySelector('.product__quantity-value').textContent);
+  }
+}
+
+inc.forEach(function(val) {
+  val.addEventListener('click', incHandler);
+})
+
+dec.forEach(function(val) {
+  val.addEventListener('click', decHandler);
+})
+
+addToCarts.forEach(function(newCart) {
+  newCart.addEventListener('click', addProduct)
+})
