050ffb7d175e3c0ae82f2ae3ffaa54d55622267e
Sergey Darvay <sdarvay@ld.yandex.ru>
2020-04-29 14:59:10 +0300

    'hz'

--- document-structure/cart/task.js
+++ document-structure/cart/task.js
@@ -21,7 +21,11 @@ const addProduct = function(event) {
   let newProd = document.createElement('div');
   newProd.className = "cart__product";
   newProd.dataset.id = curProduct.dataset.id;
-  newProd.innerHTML = '<img class="cart__product-image" src=' + curProduct.querySelector('.product__image').src + '> <div class="cart__product-count">' + +(curProduct.querySelector('.product__quantity-value').textContent) +'</div>';
+  newProd.innerHTML = '<img class="cart__product-image" src=' 
+  + curProduct.querySelector('.product__image').src + 
+  '> <div class="cart__product-count">'
+   + +(curProduct.querySelector('.product__quantity-value').textContent) +'</div>';
+   
   let alreadyExist = 0;
   if (curProduct.querySelector('.product__quantity-value').textContent != "0") {
     for (i = 0; i < cart.children.length; i++) {
