c74b99c5fff3a803d3c5a668bdc8c614c6b71df0
Sergey Darvay <sdarvay@ld.yandex.ru>
2020-05-10 14:32:04 +0300

    'krivaya_3ya'

---
+++ async-requests/progressbar/task.js
@@ -0,0 +1,20 @@
+const progress = document.getElementById( 'progress' );
+let form = document.getElementById('form');
+form.addEventListener('submit', (e) => {
+  let formData = new FormData(form);
+  let xhr = new XMLHttpRequest()
+  xhr.open('GET', 'https://netology-slow-rest.herokuapp.com/upload.php');
+  xhr.send(formData);
+  xhr.onprogress = function(event) {
+    console.log(event.lengthComputable);
+    if (event.lengthComputable) {
+      progress.value = (event.loaded/event.total);
+    }
+  }
+  e.preventDefault();
+});
+
+
+
+
+
