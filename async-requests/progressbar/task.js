9f9e5e8944aa0e06e546ddbdc9d17660d220a8d3
Sergey Darvay <sdarvay@yandex-team.ru>
2020-05-22 18:31:43 +0300

    'finaly'

--- async-requests/progressbar/task.js
+++ async-requests/progressbar/task.js
@@ -4,10 +4,11 @@ let form = document.getElementById('form');
 form.addEventListener('submit', (e) => {
   let formData = new FormData(form);
   let xhr = new XMLHttpRequest()
+  xhr.upload.addEventListener("progress", function (event) {
+    progress.value = event.loaded / event.total;
+  });
   xhr.open('POST', 'https://netology-slow-rest.herokuapp.com/upload.php');
   xhr.send(formData);
-  xhr.onprogress = function(event) {
-    progress.value = (event.loaded/100000000);  
-  };
+  
   e.preventDefault();
 });
